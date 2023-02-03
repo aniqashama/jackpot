fa93682f6d3030859172151f604442688354aa59
aniqashama <aniqa2097@gmail.com>
2023-02-03 18:08:29 +0600

    jackpot

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    // Query Part Start
+    $('.q_one').click(function(){
+      $('.a_one').toggle();
+    });
+    $('.q_two').click(function(){
+      $('.a_two').toggle();
+    });
+    $('.q_three').click(function(){
+      $('.a_three').toggle();
+    });
+    $('.q_four').click(function(){
+      $('.a_four').toggle();
+    });
+      // Query Part End
+});
